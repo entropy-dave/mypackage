6a3a29d4d40eddb1a07b843b829280a5bed2e5ba
Schönenberger <david.schoenenberger@empa.ch>
2023-05-05 12:44:28 +0200

    Initial commit

---
+++ mypackage/.ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,7 @@
+def distance2(p1: "Point", p2: "Point") -> float:
+    return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+
+#p1 = Point(0, 0)
+#p2 = Point(3, 4)
+
+#print(f"The distance between `p1` and `p2` is {distance(p1, p2)}")
